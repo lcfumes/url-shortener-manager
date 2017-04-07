3869815f4c1ca2c759f6cd88ca0d0f2d174e8993
Luiz Fumes <lcfumes@gmail.com>
2017-04-07 14:29:51 +0000

    Routes

--- config/routes.js
+++ config/routes.js
@@ -16,19 +16,6 @@ module.exports = [
   },
   {
     method: '*',
-    path: '/js/{all*}',
-    handler: {
-        proxy: {
-        host: 'localhost',
-        port: 3030,
-        protocol: 'http',
-        passThrough: true,
-        redirects: 5
-      }
-    }
-  },
-  {
-    method: '*',
     path: '/favicon.ico',
     handler: {
         proxy: {
@@ -51,6 +38,52 @@ module.exports = [
         passThrough: true,
         redirects: 5
       }
+    }
+  },
+  {
+    method: '*',
+    path: '/{folder}/{file}.{ext}',
+    handler: {
+        proxy: {
+        host: 'localhost',
+        port: 3030,
+        protocol: 'http',
+        passThrough: true,
+        redirects: 5
+      }
+    }
+  },
+  {
+    method: '*',
+    path: '/{folder*2}/{file}.{ext}',
+    handler: {
+        proxy: {
+        host: 'localhost',
+        port: 3030,
+        protocol: 'http',
+        passThrough: true,
+        redirects: 5
+      }
+    }
+  },
+  { 
+    method: '*',
+    path: '/{file}.{ext}',
+    handler: { 
+        proxy: {
+        host: 'localhost',
+        port: 3030,
+        protocol: 'http',
+        passThrough: true,
+        redirects: 5
+      }
+    }
+  },
+  {
+    method: "GET",
+    path: '/51CB41C126E83982F31DE4D199006F1C.txt',
+    handler: (request, reply) => {
+      reply('E405FA7B4193FC3599FA3023DE2CDEDE52C4C39C comodoca.com');
     }
   },
   {
